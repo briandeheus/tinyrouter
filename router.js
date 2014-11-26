cd6460d2fdbb5bbe0ba2f21ffed4267f3faa8dd1
Brian de Heus <briandeheus@gmail.com>
2014-11-26 10:50:35 +0900

    Update router.js
    Removed some silly white spaces.

--- router.js
+++ router.js
@@ -6,11 +6,11 @@ var Router = function () {
 
 Router.prototype.addRoute = function (route, func) {
 
-	var ident       = route.split('/:')[0] || route;
+	var ident = route.split('/:')[0] || route;
 	this._routes.push({
-		ident: ident,
-		func: func,
-		parts: route.split('/').length,
+		ident:     ident,
+		func:      func,
+		parts:     route.split('/').length,
 		parmsFrom: ident.split('/').length
 	});
 
@@ -25,6 +25,7 @@ Router.prototype.find = function (path) {
 	for (var i = 0, l = this._routes.length; i < l; i++) {
 
 		var route = this._routes[i]
+
 		if (ident.indexOf(route.ident) === 0 && partsl === route.parts) {
 			route.func.apply(this, parts.splice(route.parmsFrom));
 			return;
